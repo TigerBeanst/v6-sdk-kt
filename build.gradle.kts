c549901a435d22d8a88d7773935bd4d8a257b602
TigerBeanst <i@tiger.fail>
2024-12-17 01:02:08 +0800

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    `maven-publish`
 }
 
 group = "fail.tiger"
@@ -13,6 +14,24 @@ dependencies {
     implementation("com.squareup.wire:wire-runtime:5.0.0-alpha01")
     implementation("com.squareup.wire:wire-grpc-client:5.0.0-alpha01")
     testImplementation(kotlin("test"))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/tigerbeanst/v6-sdk-kt")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
 
 tasks.test {
